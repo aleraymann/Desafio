82cb635e4445caec38974b4d4ce410e9a0f12c50
aleraymann <aleraymann@gmail.com>
2019-04-11 17:36:32 -0300

    limpeza das pastas

--- storage/framework/views/30a1ee1250e30468e3639905f056416d9a627fb5.php
+++ storage/framework/views/30a1ee1250e30468e3639905f056416d9a627fb5.php
@@ -14,82 +14,129 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-
     <!-- Styles -->
     <link href="<?php echo e(asset('css/app.css')); ?>" rel="stylesheet">
 </head>
 
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-dark bg-dark fluid">
-            <div class="container">
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto">
-                    <!-- Authentication Links -->
-                    <?php if(auth()->guard()->guest()): ?>
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?php echo e(route('login')); ?>"><?php echo e(__('Login')); ?></a>
-                    </li>
-                    <?php if(Route::has('register')): ?>
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?php echo e(route('register')); ?>"><?php echo e(__('Register')); ?></a>
-                    </li>
-                    <?php endif; ?>
-                    <?php else: ?>
-                    <img src="/img/pp.png">
-                </ul>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="/home"> Home</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/user">Users</a>
-                        </li>
-                        <ul class="navbar-nav dropdown mr-auto">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Products
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="<?php echo e(url('/newProduct')); ?>">New Product</a>
-                                <a class="dropdown-item" href="<?php echo e(url('/products')); ?>">View Products</a>
-                            </div>
-                        </ul>
-                    </ul>
-                    <ul class="navbar-nav collapse navbar-collapse" id="userLogin">
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                <?php echo e(Auth::user()->name); ?> <span class="caret"></span>
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="<?php echo e(route('logout')); ?>" onclick="event.preventDefault();
-                                                        document.getElementById('logout-form').submit();">
-                                    <?php echo e(__('Logout')); ?>
-
-                                </a>
-                                <form id="logout-form" action="<?php echo e(route('logout')); ?>" method="POST"
-                                    style="display: none;">
-                                    <?php echo csrf_field(); ?>
-                                </form>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
+<body id="grad1">
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <a class="navbar-brand" href="http://www.letsgrow.com.br" target="blank"><img src="/img/pp.png"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
+            aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul class="navbar-nav mr-auto">
+                <?php if(auth()->guard()->guest()): ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo e(route('login')); ?>"><?php echo e(__('Login')); ?></a>
+                </li>
+                <?php if(Route::has('register')): ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo e(route('register')); ?>"><?php echo e(__('Register')); ?></a>
+                </li>
                 <?php endif; ?>
-            </div>
-    </div>
-    </nav>
+                <?php else: ?>
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i><u>
+                                <img src="/img/you.png"> <?php echo e(Auth::user()->name); ?> </i></u><span class="caret"></span>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="<?php echo e(route('logout')); ?>" onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+                            <img src="/img/sair.png">
+                            <?php echo e(__('Logout')); ?>
 
+                        </a>
+                        <form id="logout-form" action="<?php echo e(route('logout')); ?>" method="POST" style="display: none;">
+                            <?php echo csrf_field(); ?>
+                        </form>
+                    </div>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="/home"><img src="/img/homepage.png"> <b>Home</b></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="/user"><img src="/img/users.png"> <b>Users</b></a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle active" id="navbarDropdown" data-toggle="dropdown"><img
+                            src="/img/prod.png"> <b>Products</b></a>
+                    <div class="dropdown-menu" aria-labelledby="dropdown01">
+                        <a class="nav-item dropdown-item" href="<?php echo e(url('/products')); ?>"><img src="/img/list.png">
+                            Product List</a>
+                        <a class="nav-item dropdown-item" href="<?php echo e(url('/newProduct')); ?>"><img src="/img/add.png"> New
+                            Product</a>
+                    </div>
+                </li>
+            </ul>
+            <button class="btn btn-outline-primary " id="myBtn">Find Us</button>
+            <div id="myModal" class="modal">
+                <div class="modal-content">
+                    <h4>Let's - Sistemas Inteligentes</h5>
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe class="embed-responsive-item"
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3604.3860350258446!2d-51.467073885534816!3d-25.39188558380598!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ef36304a759317%3A0x9445763b5bcfef16!2sR.+XV+de+Novembro%2C+7286+-+Centro%2C+Guarapuava+-+PR%2C+85010-000!5e0!3m2!1spt-BR!2sbr!4v1554488185621!5m2!1spt-BR!2sbr"
+                                width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                        </div>
+                        <ul>
+                            <li>
+                                <h5>Street XV de novembro, 7286 - 2º floor, room 5;</h5>
+                            </li>
+                            <li>
+                                <h5>Work at: Mon - Fri das 9:00 AM - 18:00 PM;</h5>
+                            </li>
+                            <li>
+                                <h5> Phones: TIM:42 9.9973-8042 / VIVO:42 9.9142-7446;</h5>
+                            </li>
+                        </ul>
+                        <span class="close text-right"> &times;</span>
+                </div>
+            </div>
+        </div>
+        <?php endif; ?>
+        </div>
+    </nav>
     <main class="py-4">
         <?php echo $__env->yieldContent('content'); ?>
-
     </main>
     </div>
+    <footer class="text-muted">
+        <div class="container">
+            <p class="float-right">
+                <a href="#"><img src="/img/up.png"> Back to top</a>
+            </p>
+            <p>Dev: @aleraymann  ©2019</p>
+            <p>
+                <p>Send me an email: <a href="mailto:aleraymann@gmail.com" target="blank"><img src="/img/gmail.png"></a>
+                    or visit <a href="https://www.facebook.com/alessandro.raymann" target="blank"><img
+                            src="/img/face.png"></a>
+                    / <a href="https://www.instagram.com/aleraymann/" target="blank"><img src="/img/insta.png"></a>
+                </p>
+            </p>
+        </div>
+    </footer>
     <script src="<?php echo e(asset('js/app.js')); ?>" defer></script>
     <?php echo $__env->yieldContent('javascript'); ?>
+    <script>
+    var modal = document.getElementById('myModal');
+    var btn = document.getElementById("myBtn");
+    var span = document.getElementsByClassName("close")[0];
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+    </script>
 </body>
 
 </html>
--- storage/framework/views/d7ac7a70f7fad29fe483785d73448a58ef48b972.php
+++ storage/framework/views/d7ac7a70f7fad29fe483785d73448a58ef48b972.php
@@ -1,9 +1,24 @@
-<?php /* C:\Users\Alessandro\Documents\laravel\Desafio2\resources\views/users.blade.php */ ?>
+<?php /* /home/aleraymann/Git-lets/Desafio/resources/views/users/users.blade.php */ ?>
+
 <?php $__env->startSection('content'); ?>
 
-<div class="card-body">
-    <h5 class="card-title">Users</h5>
-    <table class="table table-bordered table-hover" id="tableUsers">
+<div class="card-body" id="app">
+    <div>
+    <h5>Search Users:
+        <form action="/user" method="POST" class="form form-inline">
+        <?php echo csrf_field(); ?>
+            <input type="text" name="id" class="form-control" placeholder="ID">
+            <input type="text" name="name" class="form-control" placeholder="Name">
+            <input type="text" name="city" class="form-control" placeholder="City">
+            <input type="text" name="state" class="form-control" placeholder="State">
+            <input type="text" name="district" class="form-control" placeholder="District">
+
+            <button type="submit" class="btn btn-primary">Search</button>
+        </form>
+    </div>
+    <h5 class="card-title">Registered Users</h5>
+    <?php echo $__env->make('components.flash-message', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+    <table class="table table-bordered table-hover col-md-3" id="tableUsers">
         <thead>
             <tr>
                 <th>Id</th>
@@ -16,6 +31,7 @@
                 <th>Number</th>
                 <th>Complement</th>
                 <th>District</th>
+                <th></th>
             </tr>
         </thead>
         <tbody>
@@ -32,7 +48,8 @@
                 <td><?php echo e($i->complement); ?></td>
                 <td><?php echo e($i->district); ?></td>
                 <td>
-                    <a href='/user/delete/<?php echo e($i->id); ?>'><img src="/img/delete.png"></a>
+                    <a href='/user/delete/<?php echo e($i->id); ?>' onclick="return confirm('Confirma Exclusão do Registro?')"><img
+                            src="/img/delete.png"></a>
                 </td>
             </tr>
             <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
