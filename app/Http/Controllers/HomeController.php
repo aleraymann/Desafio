1d1634886202f5b37fe8f59018dd3cfa061adb14
aleraymann <aleraymann@gmail.com>
2019-04-18 17:22:36 -0300

    Envio de email informativo sobre acesso

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Events\HomeEvent;
 
 class HomeController extends Controller
 {
---
+++ resources/views/emails/novoacesso.blade.php
@@ -0,0 +1,16 @@
+<html>
+
+<body>
+
+    <img width="10%" height="10%" src="{{$message->embed( public_path(). '/img/laravel.png')}}">
+    <h3>Desafio_Lets</h3>
+    </div>
+    <h2> Seja Bem Vindo(a), {{$nome}}</h2>
+    <p> Este é um email informativo:</p>
+    <p> Você acabou de logar usando o email:{{$email}}</p>
+    <p> Data/hora do acesso:{{$datahora}}</p>
+    <div>
+
+</body>
+
+</html>
